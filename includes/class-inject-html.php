3b4442d9ed54fb23499c1efbe0c4331cd126bfc4
frankremmy <princeugo.remix@gmail.com>
2024-11-18 06:01:16 +0100

    HTML Snippet Handler: Fixes #6

--- includes/class-inject-html.php
+++ includes/class-inject-html.php
@@ -1,5 +1,21 @@
 <?php
 
-class Class_Inject_HTML {
+class Custom_Code_Manager_Inject_HTML {
+	/*
+	 * Constructor to setup hooks
+	 */
+	public function __construct() {
+		add_action( 'wp_head', array( $this, 'inject_html_code' ) );
+	}
 
+	/*
+	 * Inject the HTML code into the head tag
+	 */
+	public function inject_html_code() {
+		$html_code = get_option( 'ccm_html_code' );
+
+		if ( empty( $html_code ) ) {
+			echo $html_code;
+		}
+	}
 }
