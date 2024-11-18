ab926fa1367f39d316dc7a60322b448afd60bf97
frankremmy <princeugo.remix@gmail.com>
2024-11-18 06:04:59 +0100

    JS Snippet Handler: Fixes #7

--- includes/class-inject-js.php
+++ includes/class-inject-js.php
@@ -1,5 +1,21 @@
 <?php
 
-class Class_Inject_JS {
+class Custom_Code_Manager_Inject_JS {
+	/*
+	 * Constructor to setup hooks
+	 */
+	public function __construct() {
+		add_action( 'wp_footer', array( $this, 'inject_js_code' ) );
+	}
 
+	/*
+	 * Inject the JavaScript code snippet into the footer
+	 */
+	public function inject_js_code() {
+		$js_code = get_option( 'ccm_js_code' );
+
+		if ( ! empty( $js_code ) ) {
+			echo '<script type="text/javascript">' . $js_code . '</script>';
+		}
+	}
 }
