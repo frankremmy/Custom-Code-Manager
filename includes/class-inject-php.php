7e4d814df13efe82179db6725b674ba694e4cb0b
frankremmy <princeugo.remix@gmail.com>
2024-11-18 05:56:22 +0100

    PHP Snippet Handler: Fixes #5

--- includes/class-inject-php.php
+++ includes/class-inject-php.php
@@ -1,5 +1,26 @@
 <?php
 
-class Class_Inject_PHP {
+class Custom_Code_Manager_Inject_PHP {
+	/*
+	 * Constructor to setup the hooks
+	 */
+	public function __construct() {
+		add_action( 'init', array( $this, 'inject_php_code' ) );
+	}
 
+	/*
+	 * Inject PHP code snippet
+	 */
+	public function inject_php_code() {
+		$php_code = get_option( 'ccm_php_code' );
+
+		if ( ! empty( $php_code ) ) {
+			try {
+				eval( $php_code );
+			} catch ( Exception $e ) {
+				// Handle any errors in a secure way, like logging
+				error_log( 'Custom Code Manager Error: ' . $e->getMessage() );
+			}
+		}
+	}
 }
